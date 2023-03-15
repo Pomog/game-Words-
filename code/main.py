0362288b78db4ad71df8b43d4b95d0cc4e85e6b6
Pomogadryg <denver1033@gmail.com>
2023-03-15 13:17:09 +0200

    minor changes

--- code/main.py
+++ code/main.py
@@ -1,10 +1,10 @@
 from inputProcessor import InputProcessor
 from userRegistration import UserRegistration
 from login import Login
+import submitting_words_into_db
+import random
 
-player1_score=0
-player1_name=""
-player2_score=0
+
 
 log_in_check=False
 while not log_in_check:
@@ -27,22 +27,61 @@ while not log_in_check:
             if ask2.lower()=="n":
                 print("ok, bye bye!")
                 log_in_check=True
+    else:
+        log_in_check=True
 
 
-game_over=False
+list_words = submitting_words_into_db.w
+print(list_words)
+
+played_words = []
+
+end_game = False
 
 
+def player_move():
+    print("To stop playing, do not enter the word.")
+    entered_word = input("Enter your word:  ")
+    if entered_word == '':
+        exit()
+    entered_word = entered_word.lower()
 
-while not game_over:
-    player1=InputProcessor(player1_name[])
+    if entered_word not in played_words:
+        if entered_word in list_words:
+            if len(played_words):
+                last_word = played_words[-1]
+                last_letter = last_word[-1]
+                if entered_word[0] == last_letter:
+                    played_words.append(entered_word)
+                    letters = [entered_word[0], entered_word[-1]]
+                    print(entered_word, '|')
+                    return entered_word, letters
+                else:
+                    print(f"The word should start with letter: {last_letter}")
+                    player_move()
+            else:
+                played_words.append(entered_word)
+                letters = [entered_word[0], entered_word[-1]]
+                print(entered_word, '|')
+        else:
+            print("Such word does not exist in our db, try another one...")
+            player_move()
+    else:
+        print("already played, type another word")
+        player_move()
 
 
-    
+def pc_move():
+    last_word = played_words[-1]
+    last_letter = last_word[-1]
+    for w in list_words:
+        if w[0] == last_letter[-1]:
+            if w not in played_words:
+                played_words.append(w)
+                print(w, '|')
+                return w
 
 
-
-# stop_playing=False
-
-# while not stop_playing:
-#     new_word=new_input.input_request()    
-#     stop_playing=True
+while not end_game:
+    player_move()
+    pc_move()
